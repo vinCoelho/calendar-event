ac5e663b7729c6c6ba28eb0587dbf3e62e421cb9
Vinícius Coelho <viniciusraposo@gmail.com>
2023-07-04 20:47:51 -0300

    teste: iOS

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ console.log(md.match('playstation|xbox')); // false
 
 const path = window.location.href;
 
-// Extract event details from the pathname
 const [, , loc, description, summary, dtstart, dtend] = path.split("#/");
 
 console.log("location", decodeURIComponent(loc));
@@ -23,18 +22,42 @@ console.log("summary", decodeURIComponent(summary));
 console.log("dtstart", decodeURIComponent(dtstart));
 console.log("dtend", decodeURIComponent(dtend));
 
-if (md.mobile()) {
-  const encodedSummary = encodeURIComponent(summary);
-  const encodedLocation = encodeURIComponent(loc);
-  const encodedDescription = encodeURIComponent(description);
-  const encodedDTStart = encodeURIComponent(dtstart);
-  const encodedDTEnd = encodeURIComponent(dtend);
+const encodedSummary = encodeURIComponent(summary);
+const encodedLocation = encodeURIComponent(loc);
+const encodedDescription = encodeURIComponent(description);
+const encodedDTStart = encodeURIComponent(dtstart);
+const encodedDTEnd = encodeURIComponent(dtend);
 
-  const link = `data:text/calendar;charset=utf-8,BEGIN:VCALENDAR%0AVERSION:2.0%0ABEGIN:VEVENT%0ADTSTART:${encodedDTStart}%0ADTEND:${encodedDTEnd}%0ASUMMARY:${encodedSummary}%0ADESCRIPTION:${encodedDescription}%0ALOCATION:${encodedLocation}%0AEND:VEVENT%0AEND:VCALENDAR`;
-
-  decodeURIComponent(link);
+if (md.is('iPhone')) {
+  const link = `data:text/calendar;charset=utf-8,BEGIN:VCALENDAR
+VERSION:2.0
+X-WR-CALNAME:Reserva Veículo
+BEGIN:VEVENT
+DTSTART:${encodedDTStart}
+DTEND:${encodedDTEnd}
+LOCATION:${encodedLocation}
+SUMMARY:Reserva Veículo Unidas
+DESCRIPTION:${encodedDescription}
+PRIORITY:3
+METHOD:REQUEST
+X-APPLE-STRUCTURED-LOCATION;VALUE=URI;X-APPLE-MAPKIT-HANDLE=ABUID:1234567890:placemark:abc123:0:0:0:0:0:0:0.0:0.0:0:ROD MG 10 KM 39 - 0 - AEROPORTO INT. TANCREDO N - CONFINS - MG
+END:VEVENT
+BEGIN:VALARM
+ACTION:DISPLAY
+TRIGGER;VALUE=DURATION:-PT20M
+DESCRIPTION: Reserva Veículo - Unidas Aluguel de Carros
+END:VALARM
+END:VCALENDAR`;
   
   window.location.href = link;
-} else {
+} else if (!md.mobile()) {
   window.location.href = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${decodeURIComponent(summary)}&dates=${decodeURIComponent(dtstart)}/${decodeURIComponent(dtend)}&details=${decodeURIComponent(description)}&location=${decodeURIComponent(loc)}&sf=true&output=xml`;
 }
+else {
+  const link = `data:text/calendar;charset=utf-8,BEGIN:VCALENDAR%0AVERSION:2.0%0ABEGIN:VEVENT%0ADTSTART:${encodedDTStart}%0ADTEND:${encodedDTEnd}%0ASUMMARY:${encodedSummary}%0ADESCRIPTION:${encodedDescription}%0ALOCATION:${encodedLocation}%0AEND:VEVENT%0AEND:VCALENDAR`;
+
+  window.location.href = link;
+}
+
+
+// https://vincoelho.github.io/calendar-event/#/event#/ROD%20MG%2010%20KM%2039%20-%20-%20AEROPORTO%20INT.%20TANCREDO%20NEVES%20-%20CONFIS%20-%20MG#/-%20Retirada:%2017/03/2023%20às%2018:39%0A-%20Devolução:%2018/03/2023%20às%2010:00#/Reserva%20Veículo%20-%20Unidas#/20230622T230000#/20230623T230000
